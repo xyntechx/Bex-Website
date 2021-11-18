fb7534377283ee2459a7fb1fa5e106a017d532d6
xyntechx <xyntechx@gmail.com>
2021-11-18 10:58:16 +0800

    🎉 Initialise pages

---
+++ pages/fiction.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.sass";
+
+const Fiction: NextPage = () => {
+    return (
+        <h1>Fiction</h1>
+    );
+};
+
+export default Fiction
---
+++ pages/reviews.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.sass";
+
+const Reviews: NextPage = () => {
+    return (
+        <h1>Reviews</h1>
+    );
+};
+
+export default Reviews;
