ff4e90f3bc7c80bfa93785b9538c8addbb5a2e7d
xyntechx <xyntechx@gmail.com>
2021-12-01 16:42:21 +0800

    ✨ Artworks page

--- pages/artworks.tsx
+++ pages/artworks.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.sass";
+import grid from "../styles/Grid.module.sass";
 
 const Artworks: NextPage = () => {
     return (
@@ -17,6 +18,186 @@ const Artworks: NextPage = () => {
 
             <main className={styles.main}>
                 <h1>Artworks</h1>
+                <section className={grid.container}>
+                    <section className={grid.artworkGrid}>
+                        <div className={grid.card}>
+                            <Image
+                                src="/artwork-1.png"
+                                alt="Artwork 1"
+                                layout="fill"
+                                className={grid.image}
+                            />
+                        </div>
+                        <div className={grid.card}>
+                            <Image
+                                src="/artwork-2.png"
+                                alt="Artwork 1"
+                                layout="fill"
+                                className={grid.image}
+                            />
+                        </div>
+                        <div className={grid.card}>
+                            <Image
+                                src="/artwork-3.png"
+                                alt="Artwork 1"
+                                layout="fill"
+                                className={grid.image}
+                            />
+                        </div>
+                        <div className={grid.card}>
+                            <Image
+                                src="/artwork-4.png"
+                                alt="Artwork 1"
+                                layout="fill"
+                                className={grid.image}
+                            />
+                        </div>
+                        <div className={grid.card}>
+                            <Image
+                                src="/artwork-5.png"
+                                alt="Artwork 1"
+                                layout="fill"
+                                className={grid.image}
+                            />
+                        </div>
+                        <div className={grid.card}>
+                            <Image
+                                src="/artwork-6.png"
+                                alt="Artwork 1"
+                                layout="fill"
+                                className={grid.image}
+                            />
+                        </div>
+                        <div className={grid.card}>
+                            <Image
+                                src="/artwork-7.png"
+                                alt="Artwork 1"
+                                layout="fill"
+                                className={grid.image}
+                            />
+                        </div>
+                        <div className={grid.card}>
+                            <Image
+                                src="/artwork-8.png"
+                                alt="Artwork 1"
+                                layout="fill"
+                                className={grid.image}
+                            />
+                        </div>
+                        <div className={grid.card}>
+                            <Image
+                                src="/artwork-9.png"
+                                alt="Artwork 1"
+                                layout="fill"
+                                className={grid.image}
+                            />
+                        </div>
+                        <div className={grid.card}>
+                            <Image
+                                src="/artwork-10.png"
+                                alt="Artwork 1"
+                                layout="fill"
+                                className={grid.image}
+                            />
+                        </div>
+                        <div className={grid.card}>
+                            <Image
+                                src="/artwork-11.png"
+                                alt="Artwork 1"
+                                layout="fill"
+                                className={grid.image}
+                            />
+                        </div>
+                        <div className={grid.card}>
+                            <Image
+                                src="/artwork-12.gif"
+                                alt="Artwork 1"
+                                layout="fill"
+                                className={grid.image}
+                            />
+                        </div>
+                        <div className={grid.card}>
+                            <Image
+                                src="/artwork-13.gif"
+                                alt="Artwork 1"
+                                layout="fill"
+                                className={grid.image}
+                            />
+                        </div>
+                        <div className={grid.card}>
+                            <Image
+                                src="/artwork-14.png"
+                                alt="Artwork 1"
+                                layout="fill"
+                                className={grid.image}
+                            />
+                        </div>
+                        <div className={grid.card}>
+                            <Image
+                                src="/artwork-15.png"
+                                alt="Artwork 1"
+                                layout="fill"
+                                className={grid.image}
+                            />
+                        </div>
+                        <div className={grid.card}>
+                            <Image
+                                src="/artwork-16.png"
+                                alt="Artwork 1"
+                                layout="fill"
+                                className={grid.image}
+                            />
+                        </div>
+                        <div className={grid.card}>
+                            <Image
+                                src="/artwork-17.png"
+                                alt="Artwork 1"
+                                layout="fill"
+                                className={grid.image}
+                            />
+                        </div>
+                        <div className={grid.card}>
+                            <Image
+                                src="/artwork-18.png"
+                                alt="Artwork 1"
+                                layout="fill"
+                                className={grid.image}
+                            />
+                        </div>
+                        <div className={grid.card}>
+                            <Image
+                                src="/artwork-19.png"
+                                alt="Artwork 1"
+                                layout="fill"
+                                className={grid.image}
+                            />
+                        </div>
+                        <div className={grid.card}>
+                            <Image
+                                src="/artwork-20.png"
+                                alt="Artwork 1"
+                                layout="fill"
+                                className={grid.image}
+                            />
+                        </div>
+                        <div className={grid.card}>
+                            <Image
+                                src="/artwork-21.png"
+                                alt="Artwork 1"
+                                layout="fill"
+                                className={grid.image}
+                            />
+                        </div>
+                        <div className={grid.card}>
+                            <Image
+                                src="/artwork-22.png"
+                                alt="Artwork 1"
+                                layout="fill"
+                                className={grid.image}
+                            />
+                        </div>
+                    </section>
+                </section>
             </main>
         </div>
     );
