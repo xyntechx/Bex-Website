37e4ec4547b6875baeb6930c26484e56bd4564a8
xyntechx <xyntechx@gmail.com>
2021-12-03 13:37:22 +0800

    🔖 03/12

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,10 +24,50 @@ const Home: NextPage = () => {
                     width={300}
                     height={300}
                 />
-                <p className={styles.text}>About</p>
-                <Link href="/artworks">
-                    <a className={styles.link}>Check out my art</a>
-                </Link>
+                <br />
+                <br />
+                <p className={styles.intro}>
+                    Hello there 👋 Welcome to my website
+                </p>
+                <p className={styles.text}>
+                    I&apos;m Rebecca, a positive, energetic, and warm teen who
+                    loves{" "}
+                    <Link href="/artworks">
+                        <a target="_blank" className={styles.link}>
+                            art
+                        </a>
+                    </Link>
+                    . In fact, I am a member of the Art Club of Raffles Girls'
+                    School (Secondary) where I explore and develop my artistic
+                    skills. Sometimes, you can spot me drawing either digitally
+                    or on paper, or listening to pop music. More often,
+                    it&apos;s both! I also enjoy watching K-Dramas, Hollywood
+                    films, as well as animations - be it Anime or Disney.
+                    Frankly, I see myself as someone who doesn&apos;t have a
+                    specific preference for anything such as food, movies,
+                    fashion, aesthetics or even art style; I&apos;m someone who
+                    simply loves to experiment with and have a little taste of
+                    everything, making me somewhat of a jack-of-all-trades. This
+                    may be caused by my endless curiosity and restlessness (or
+                    my indecisiveness 👀). Either way, I&apos;m someone who
+                    constantly looks for new experiences, never settling with
+                    just one constant.
+                </p>
+                <br />
+                <br />
+                <div className={styles.social}>
+                    Follow me on{" "}
+                    <Link href="https://www.instagram.com/olegna_s/">
+                        <a target="_blank">
+                            <Image
+                                src="/instagram.png"
+                                alt="Instagram"
+                                width={20}
+                                height={20}
+                            />
+                        </a>
+                    </Link>
+                </div>
             </main>
         </div>
     );
