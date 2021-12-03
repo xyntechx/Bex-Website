0d7901bbad60b86ed551ae5b1fd83596e716e094
xyntechx <xyntechx@gmail.com>
2021-12-03 11:54:29 +0800

    🔥 Remove unused links

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,35 +18,16 @@ const Home: NextPage = () => {
 
             <main className={styles.main}>
                 <h1 className={styles.title}>Rebecca Iskandar</h1>
-                <sub>
-                    Hmm... the picture seems to be bouncing... why don&apos;t you
-                    hover over it?
-                </sub>
-                <div className={styles.container}>
-                    <div className={styles.smallContainer}>
-                        <Link href="/about">
-                            <a className={styles.link}>About</a>
-                        </Link>
-                        <Link href="/fiction">
-                            <a className={styles.link}>Fiction</a>
-                        </Link>
-                    </div>
-                    <Image
-                        src="/rebecca.jpg"
-                        alt="Rebecca's Profile Picture"
-                        className={styles.bounce}
-                        width={300}
-                        height={300}
-                    />
-                    <div className={styles.smallContainer}>
-                        <Link href="/poems">
-                            <a className={styles.link}>Poems</a>
-                        </Link>
-                        <Link href="/artworks">
-                            <a className={styles.link}>Artworks</a>
-                        </Link>
-                    </div>
-                </div>
+                <Image
+                    src="/rebecca.jpg"
+                    alt="Rebecca's Profile Picture"
+                    width={300}
+                    height={300}
+                />
+                <p className={styles.text}>About</p>
+                <Link href="/artworks">
+                    <a className={styles.link}>Check out my art</a>
+                </Link>
             </main>
         </div>
     );
